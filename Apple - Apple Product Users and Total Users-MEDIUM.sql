f21e4e2c806bc210cc3a23b5dea485794ab565c2
Niluthpal Chowdhury <110477630+Niluthpal@users.noreply.github.com>
2024-09-23 17:37:36 +0530

    Update Apple - Apple Product Users and Total Users-MEDIUM.sql

--- Apple - Apple Product Users and Total Users-MEDIUM.sql
+++ Apple - Apple Product Users and Total Users-MEDIUM.sql
@@ -1,4 +1,6 @@
--- Find the number of Apple product users and the number of total users with a device and group the counts by language. Assume Apple products are only MacBook-Pro, iPhone 5s, and iPad-air. Output the language along with the total number of Apple users and users with any device. Order your results based on the number of total users in descending order.
+-- Find the number of Apple product users and the number of total users with a device and group the counts by language. 
+--Assume Apple products are only MacBook-Pro, iPhone 5s, and iPad-air. Output the language along with the total number of Apple users and users with any device. 
+--Order your results based on the number of total users in descending order.
 
 CREATE TABLE playbook_events (
     user_id INT,
